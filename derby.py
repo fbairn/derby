5647bec49a2db0d15c4867663bf7af8c6970e29e
Amos Bairn <Eylrid@Gmail.com>
2015-01-15 09:54:05 -0700

    Initial Commit

---
+++ derby.py
@@ -0,0 +1,98 @@
+#determine which boys race in which races in a pinewood derby
+import random
+
+class Boy:
+    def __init__(self, id):
+        self.id = id            #id can be any hashable type,
+                                #but each boy must have unique id
+        self.racecount = 0      #number of times raced
+        self.racedagainst = {}  #number of times raced against other boys
+
+    def __repr__(self):
+        '''return a string representation'''
+        return '<Boy: %s>'%str(self.id)
+
+    def race(self, others):
+        '''add 1 to race counts'''
+        self.racecount += 1
+        for other in others:
+            self.racedagainst[other.id] = self.racedagainst.get(other.id, 0) + 1
+
+    def info(self):
+        '''print some info about race counts'''
+        print 'id:', self.id
+        print 'races:', self.racecount
+        print 'raced against:'
+        for other in self.racedagainst:
+            count = self.racedagainst[other]
+            print '-', other, count
+
+    def racedagainstcount(self, others):
+        '''return the total number of times the boy has raced against others'''
+        count = 0
+        for other in others:
+            count += self.racedagainst.get(other.id, 0)
+        return count
+
+
+def race_boys(race):
+    '''record race for each boy'''
+    for boy in race:
+        others = race[:]
+        others.remove(boy)
+        boy.race(others)
+
+def get_boys(numberofboys):
+    return [Boy(i) for i in range(numberofboys)]
+
+def generate_races(boys, racesize):
+    '''
+    yield repeated races for the given number of boys
+    and the number of cars in each race
+    '''
+    while True:
+        left = boys[:]                          #create a copy of the boys list for each race
+        random.shuffle(left)                    #mix it up a bit
+        left.sort(key = lambda x: x.racecount)  #take boys with fewest races first
+        race = [left.pop(0)]
+        for i in range(racesize-1):
+            if not left: break                  #no boys left to add to race
+            left.sort(key = lambda x: x.racedagainstcount(race)) #take boys with fewest races in common with boys already in the race
+            race.append(left.pop(0))
+
+        race_boys(race) #mark the boys as having raced
+        yield race
+
+def race(numberofboys, racesize):
+    boys = get_boys(numberofboys)
+    for race in generate_races(boys, racesize):
+        print race
+        for boy in race:
+            boy.info()
+        raw_input('press enter for next race')
+
+def get_races(numberofboys, racesize, heats):
+    boys = get_boys(numberofboys)
+    racegen = generate_races(boys, racesize)
+    races = []
+    for race in racegen:
+        races.append(race)
+        if all([b.racecount>=heats for b in boys]):
+            break
+    return races
+
+def races_to_csv(races, filename):
+    with open(filename, 'w') as file:
+        for race in races:
+            file.write(','.join([str(b.id) for b in race])+'\n')
+
+def main():
+    numberofboys = int(raw_input('Number of boys? '))
+    racesize = int(raw_input('Number of boys per race? '))
+    heats = int(raw_input('Minimum number of races for each boy? '))
+    races = get_races(numberofboys, racesize, heats)
+    filename = raw_input('Output filename? ')
+    races_to_csv(races, filename)
+
+if __name__ == '__main__':
+    main()
